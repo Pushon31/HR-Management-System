025dcfcbc4a33dc19feb0292de008f6d50cb16dd
pushon31 <syednooresams@gmail.com>
2025-09-09 01:46:34 +0600

    first commit

---
+++ src/app/components/home/home.component.ts
@@ -0,0 +1,23 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-home',
+  templateUrl: './home.component.html',
+  styleUrls: ['./home.component.scss']
+})
+export class HomeComponent {
+  allLogos: string[] = [
+    'https://static.vecteezy.com/system/resources/previews/010/411/215/non_2x/garment-logo-design-scissors-silhouette-combined-with-building-icon-in-a-creative-concept-vector.jpg',
+    'https://cdn.dribbble.com/userupload/31896047/file/original-951b918aac642b7fb467f6749264bb03.jpg?resize=752x&vertical=center',
+    'https://img.freepik.com/free-vector/flat-fashion-repair-service-logo-template_23-2150237471.jpg?semt=ais_hybrid&w=740&q=80',
+    'https://static.vecteezy.com/system/resources/thumbnails/023/160/394/small_2x/clothing-logo-design-women-boutique-logo-vector.jpg',
+    'https://dcassetcdn.com/design_img/3931946/478969/26190393/nn86943bryt0veay50esgpdzr0_image.jpg',
+    'https://images-platform.99static.com//kkAEl8i2LzGXcQF2jN3icHhgtFA=/1766x2165:2788x3187/fit-in/500x500/projects-files/46/4623/462344/111b69c6-7bcd-44cf-a5f0-1a54d66aca3e.png',
+    'https://i.pinimg.com/474x/fd/bf/34/fdbf341a857000582800baa8c51c1205.jpg',
+    'https://cdn.dribbble.com/userupload/3160902/file/original-737b2b7d05c1dc7e73b7a3895509369b.jpg?format=webp&resize=400x300&vertical=center',
+    'https://cdn.dribbble.com/userupload/41322407/file/original-e2fa461d3b256f15b5cb8244f358ec4f.jpg?format=webp&resize=400x300&vertical=center',
+    'https://img.freepik.com/free-vector/flat-fashion-repair-service-logo-template_23-2150237471.jpg?semt=ais_hybrid&w=740&q=80',
+    'https://d1csarkz8obe9u.cloudfront.net/posterpreviews/fashion-designer-logo-template-ec8df7d0fea4954689750b9049c4aa85_screen.jpg?ts=1725044488',
+    'https://d1csarkz8obe9u.cloudfront.net/posterpreviews/fashion-designer-logo-template-ec8df7d0fea4954689750b9049c4aa85_screen.jpg?ts=1725044488'
+  ];
+}
